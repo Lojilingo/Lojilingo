dda74b736fd6d83150656c3d4564b44f3f3cc90b
Lojilingo <lojilingo@gmail.com>
2026-07-22 10:25:35 +0300

    sw.js

---
+++ sw.js
@@ -0,0 +1,44 @@
+// sw.js - Lojilingo Service Worker
+const CACHE_NAME = 'lojilingo-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+  // İhtiyaç duyduğunuz diğer statik dosyaları buraya ekleyin
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
